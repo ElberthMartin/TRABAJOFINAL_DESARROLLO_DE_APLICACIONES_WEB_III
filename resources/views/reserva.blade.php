0f4442524d851dd61a8eb9d0650850388357a3fe
ElberthMartin <1438878@senati.pe>
2023-10-05 13:59:13 -0500

    se agrego reserva

--- resources/views/reserva.blade.php
+++ resources/views/reserva.blade.php
@@ -3,29 +3,62 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Reserva</title>
+    <title>Reserva en Huánuco, Perú</title>
     <!-- Agrega tus enlaces a hojas de estilo CSS aquí -->
 </head>
 <body>
     <header>
-        <nav>
-            <ul>
-            <li><a href="/">Inicio</a></li>
-                <li><a href="#">Página 1</a></li>
-                <li><a href="#">Página 2</a></li>
-                <li><a href="#">Página 3</a></li>
-            </ul>
-        </nav>
-    </header>
-
-    <main>
-        <!-- Contenido principal de tu página -->
-    </main>
-
-    <footer>
-        <!-- Pie de página -->
-    </footer>
-
-    <!-- Agrega tus enlaces a scripts JavaScript aquí -->
+    <div id="container" class="row">
+  <h1>Travel WebApp</h1>
+  <p>Inspirado en el diseño de <a href="http://dribbble.com/MonikaMajkowska">Monika Majkowska</a>, haz clic <a href="http://dribbble.com/shots/1208466-Safari/attachments/161078">aquí</a> para ver la imagen original.</p>
+  <div id="booking" class="widget large-12 small-11 small-centered columns">
+    <div id="imagePreview" class="large-7 small-12 columns">
+      <h1>4 días de safari en Huánuco, Perú</h1>
+      <h3>Parque Nacional Serengeti</h3>
+    </div>
+    <div id="menu" class="large-5 columns">
+      <h3>Reserva de campamento en Huánuco, Perú</h3>
+      <form action="#" class="small-12 small-centered columns">
+        <label for="nombre">Nombre</label>
+        <input type="text" id="nombre" />
+        <label for="apellido">Apellido</label>
+        <input type="text" id="apellido" />
+        <label for="correo">Correo Electrónico</label>
+        <input type="email" id="correo" />
+        <label for="fechaLlegada">Fecha de llegada</label>
+        <input type="date" id="fechaLlegada" />
+        <label for="fechaRegreso">Fecha de regreso</label>
+        <input type="date" id="fechaRegreso"/>
+        <input type="checkbox" id="sinFecha"/>
+        <label for="sinFecha">Aún no tengo fechas específicas</label>
+        <label for="escogeCamp">Escoge tu campamento</label>
+        <select id="escogeCamp">
+          <option value="campamentoNo1">Zona de camping No. 01</option>
+          <option value="campamentoNo2">Zona de camping No. 02</option>
+          <option value="campamentoNo3">Zona de camping No. 03</option>
+          <option value="campamentoNo4">Zona de camping No. 04</option>
+          <option value="campamentoNo5">Zona de camping No. 05</option>
+        </select>
+        <label>¿Quiénes irán?</label>
+        <select id="adultosInvitados" class="small-5 columns">
+          <option value="1">1 Adulto</option>
+          <option value="2">2 Adultos</option>
+          <option value="3">3 Adultos</option>
+          <option value="4">4 Adultos</option>
+          <option value="5">5 Adultos</option>
+        </select>
+        <select id="niñosInvitados" class="small-5 small-offset-2 columns">
+          <option value="1">1 Niño</option>
+          <option value="2">2 Niños</option>
+          <option value="3">3 Niños</option>
+          <option value="4">4 Niños</option>
+          <option value="5">5 Niños</option>
+        </select>
+        <input type="button" value="Haz mi reserva"/>
+      </form>
+    </div>
+  </div>
+  <p>Todavía estoy trabajando en ello; hay algunos problemas con los que todavía no sé cómo lidiar.</p>
+</div>
 </body>
 </html>
